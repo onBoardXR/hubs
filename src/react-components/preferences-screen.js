93b9188c68944a10996710607f9130a5bf034015
Michael <mm8775@nyu.edu>
2022-10-18 09:41:21 -0500

    stabilized for merge with clem new structure

--- src/react-components/preferences-screen.js
+++ src/react-components/preferences-screen.js
@@ -995,7 +995,7 @@ class PreferencesScreen extends Component {
             key: "movementSpeedModifier",
             prefType: PREFERENCE_LIST_ITEM_TYPE.NUMBER_WITH_RANGE,
             min: 0,
-            max: 2,
+            max: 100,
             step: 0.1,
             digits: 1
           }
